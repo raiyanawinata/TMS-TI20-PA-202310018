ec78e563b472474ffbd4456ff37014bd5321b53e
raiyana winata <101450007+raiyanawinata@users.noreply.github.com>
2022-10-07 23:24:19 +0700

    Add files via upload

---
+++ TMS-20-TI-PA-202310018/academicservices/src/main/java/com/ibik/academicservices/academicservices/programs/ProgramsRepo.java
@@ -0,0 +1,6 @@
+package com.ibik.academicservices.academicservices.programs;
+
+import org.springframework.data.repository.CrudRepository;
+public interface ProgramsRepo extends CrudRepository<Programs, Integer> {
+    
+}
---
+++ TMS-20-TI-PA-202310018/academicservices/src/main/java/com/ibik/academicservices/academicservices/students/ProgramsStudentRepo.java
@@ -0,0 +1,7 @@
+package com.ibik.academicservices.academicservices.students;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProgramsStudentRepo extends CrudRepository<ProgramsStudent, Integer> {
+    
+}

c1eac811505e11777e6f71423cec658a1e37b759
Raiyanawinata <raiyanawinata75@gmail.com>
2022-11-19 15:41:49 +0700

    JPQL

--- academicservices/src/main/java/com/ibik/academicservices/academicservices/students/ProgramsStudentRepo.java
+++ academicservices/src/main/java/com/ibik/academicservices/academicservices/students/ProgramsStudentRepo.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ProgramsStudentRepo extends CrudRepository<ProgramsStudent, Integer> {
-    @Query("SELECT a FORM ProgramsStudent a WHERE a.firstname LIKE :name OR a.middlename LIKE :name OR a.lastname LIKE :name OR a.npm LIKE :name")
+    @Query("SELECT a FROM ProgramsStudent a WHERE a.firstname LIKE :name OR a.middlename LIKE :name OR a.lastname LIKE :name OR a.npm LIKE :name")
     public Iterable<ProgramsStudent> findProgramsStudentByName (@PathParam("name") String name);
 }

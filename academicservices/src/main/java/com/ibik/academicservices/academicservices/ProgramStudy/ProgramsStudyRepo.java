4979beb6e1755010fbe3c7d726a3ca8680a3cba9
Raiyanawinata <raiyanawinata75@gmail.com>
2022-11-19 15:45:27 +0700

    JPQL

--- academicservices/src/main/java/com/ibik/academicservices/academicservices/ProgramStudy/ProgramsStudyRepo.java
+++
@@ -1,7 +0,0 @@
-package com.ibik.academicservices.academicservices.ProgramStudy;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProgramsStudyRepo extends CrudRepository<ProgramsStudy, Integer> {
-    
-}
